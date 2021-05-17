b11ece23240b3813737ee998616f5c49af407a59
rgg7888 <rgg7888@outlook.com>
2021-05-17 14:24:10 -0600

    updating

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,7 @@
 
 if(!function_exists('page')) {
     function page (array $estructura) {
-        $page_template_1 = new Wrapper();
+        $page_template_1 = new App\wrapper\Wrapper();
         echo $page_template_1->page($estructura);
     }
 }
